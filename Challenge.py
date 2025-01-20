a76af0584a8bbc4891f6cfbbce965a923a88467c
AlysonPereira <alyson.bosch.senai14@gmail.com>
2025-01-20 16:54:13 -0300

    Update Challenge.py

--- Challenge.py
+++ Challenge.py
@@ -1,2 +1,9 @@
-# Desafio 01 - O Merge Sort
+# Desafios
 
+# Primeiro Desafio - Implemente O Merge Sort
+
+# Segundo Desafio - Implemente o Insertion Sort e Bubble Sort
+
+# Terceiro Desafio - Implemente a Busca Linear e Busca Binária 
+
+# Quarto Desafio - Pesquisar sobre complexidade de algoritmo e calcule a complexidade dos algoritmos anteriores
